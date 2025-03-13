5c0a1e9a393069004ac1f039029d362a92201ea5
Garhes <166943339+Garhes@users.noreply.github.com>
2025-03-13 16:30:11 -0500

    Update Caso_51479.sql

--- Caso_51479.sql
+++ Caso_51479.sql
@@ -1,17 +1,27 @@
 /*solicito su colaboración enviando el reporte de las notas de cada cursos con la relación de los estudiantes de la Escuela de Ingenierías 
 Ingeneria de sistemas y Ingeneria de logistica de los periodos académicos 2020-I, 2020-II y 2021-I.*/
 SELECT 
-pg.pege_id,
-pr.prog_nombre,
-pu.peun_ano,
-pu.peun_periodo
-FROM general.personageneral pg
-INNER JOIN academico.estudiantepensum ep ON ep.pege_id = pg.pege_id
-INNER JOIN academico.periodouniversidad pu ON pu.peun_id = ep.peun_id
-INNER JOIN academico.unidadprograma up ON up.unid_id = ep.unid_id
+hc.HICA_VALOR,
+pr.PROG_NOMBRE,
+pu.PEUN_PERIODO,
+pu.PEUN_ANO,
+ep.ESTP_ID,
+mt.MATE_CODIGOMATERIA, 
+mt.MATE_NOMBRE,
+gr.GRUP_NOMBRE,
+hn.NOTA_DESCRIPCION
+FROM academico.his_calificacion hc
+INNER JOIN academico.registroacademico ra ON ra.reac_id = hc.reac_id
+INNER JOIN academico.periodouniversidad pu ON pu.peun_id = ra.peun_id
+INNER JOIN academico.estudiantepensum ep ON ep.estp_id = ra.estp_id
+INNER JOIN academico.unidadprograma up ON up.unpr_id = ep.unpr_id
 INNER JOIN academico.programa pr ON pr.prog_id = up.prog_id
-WHERE 
-pu.peun_periodo IN (1,2)
-AND
+INNER JOIN academico.materia mt ON mt.mate_codigomateria = ra.mate_codigomateria  
+INNER JOIN academico.grupo gr ON gr.grup_id = ra.grup_id 
+INNER JOIN academico.his_nota hn ON hn.hino_id = hc.hino_id
+WHERE pu.peun_periodo IN (1,2)
+AND 
 pu.peun_ano IN (2020,2021)
-;
+AND
+pr.prog_nombre IN ('INGENIERÍA DE SISTEMAS','INGENIERÍA EN LOGÍSTICA')
+ORDER BY pu.peun_ano ASC
